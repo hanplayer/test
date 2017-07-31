d0b083f8371586b06a60bc098ae94650355de36f
zhihu.xu <zhihu.xu@colasoft.com.cn>
2017-07-31 10:55:28 -0400

    完成NumPlusNum的函数

--- test.cpp
+++ test.cpp
@@ -69,7 +69,7 @@ int NumMultiplyNum(struct A &first,struct A &second,struct A &result)
 		short len;
 		BitMultiplyNum(first,second.value[i],tmp,len);
 #if 0
-		printf("---------\n");
+		printf("----tmp----\n");
 		for(short j = 0;j < len ;j++)
 		{
 			printf("%d",tmp[j]);
@@ -168,6 +168,7 @@ int NumPlusNum(struct A first,struct A second,struct A &result)
 	char carry = 0;
 	char bit = 0;
 	short i;
+	memset(result.value,0,126);
 	for(i = 0;i < len; i++)
 	{
 		BitPlusBit(first.value[i],second.value[i],bit,carry);
@@ -182,6 +183,19 @@ int NumPlusNum(struct A first,struct A second,struct A &result)
 	{
 		result.len = len;
 	}
+	result.dec_len = first.dec_len;
+#if 1
+		printf("----result----\n");
+		for(short j = 0;j < 126;j++)
+		{
+			printf("%d ",result.value[j]);
+		}
+		printf("\n");
+		printf("result.len:%d\n",result.len);
+		printf("result.dec_len:%d\n",result.dec_len);
+		
+#endif
+
 	return 0;
 }
 
@@ -204,6 +218,8 @@ int CompleteZero(struct A &first,struct A &second)
 	short AppendLen = second.dec_len - first.dec_len;
 	char sec_op[126];
 	char fir_op[126];
+	memset(fir_op,0,126);
+	memset(sec_op,0,126);
 	for(int i = 0; i < AppendLen;i++)
 	{
 		sec_op[i] = 0;
@@ -217,8 +233,8 @@ int CompleteZero(struct A &first,struct A &second)
 	{
 		sec_op[j+AppendLen] = second.value[j];
 	}
-	memcpy(fir_op,first.value,126);//????
-#if 1
+	memcpy(fir_op,first.value,first.len);//????
+#if 0
 		printf("-----first----\n");
 		for(short j = 0;j < 126;j++)
 		{
@@ -226,7 +242,7 @@ int CompleteZero(struct A &first,struct A &second)
 		}
 		printf("\n");
 #endif
-#if 1
+#if 0
 		printf("-----fir_op----\n");
 		for(short j = 0;j < 126;j++)
 		{
@@ -250,6 +266,28 @@ int CompleteZero(struct A &first,struct A &second)
 	}
 	memcpy(first.value,fir_op,126);
 	memcpy(second.value,sec_op,126);
+#if 1
+		printf("-----one----\n");
+		for(short j = 0;j < 126;j++)
+		{
+			printf("%d ",first.value[j]);
+		}
+		printf("\n");
+		printf("first.len:%d\n",first.len);
+		printf("first.dec_len:%d\n",first.dec_len);
+		
+#endif
+#if 1
+		printf("-----second----\n");
+		for(short j = 0;j < 126;j++)
+		{
+			printf("%d ",second.value[j]);
+		}
+		
+		printf("\n");
+		printf("second.len:%d\n",second.len);
+		printf("second.dec_len:%d\n",second.dec_len);
+#endif
 	if(differ < 0)
 	{
 		first.len = second_len;
@@ -343,23 +381,33 @@ int main(void)
 #endif
 #if 1
 	struct A a1,a2,result;
-	a1.value[0] = 5;
-	a1.value[1] = 4;
-	a1.value[2] = 3;
-	a1.value[3] = 2;
-	a1.value[4] = 1;
+	a1.value[0] = 1;
+	a1.value[1] = 0;
+	a1.value[2] = 0;
+	a1.value[3] = 0;
+	a1.value[4] = 0;
+	a1.value[5] = 8;
+	a1.value[6] = 9;
+	a1.value[7] = 9;
+	a1.value[8] = 9;
+	a1.value[9] = 9;
 
-	a1.len = 5;
+	a1.len = 10;
 	a1.dec_len = 1;
 
-	a2.value[0] = 2;
-	a2.value[1] = 3;
-	a2.value[2] = 6;
-	a2.value[3] = 3;
-	a2.value[4] = 8;
+	a2.value[0] = 1;
+	a2.value[1] = 0;
+	a2.value[2] = 0;
+	a2.value[3] = 0;
+	a2.value[4] = 0;
+	a2.value[5] = 8;
+	a2.value[6] = 9;
+	a2.value[7] = 9;
+	a2.value[8] = 9;
+	a2.value[9] = 9;
 
-	a2.len = 5;
-	a2.dec_len = 2;
+	a2.len = 10;
+	a2.dec_len = 1;
 	NumMultiplyNum(a1,a2,result);
 	printf("result.len:%d\n",result.len);
 	printf("result:\n");
